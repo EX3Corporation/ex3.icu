0a64270c1626e47c99102329171db40c7745265d
exerinity <me@ex3.lol>
2026-03-22 17:04:40 +1100

    m

--- files/home.js
+++ files/home.js
@@ -1,8 +1,7 @@
 console.log("Initializing...");
 const servercount = document.getElementById('servercount');
 servercount.innerHTML = `<div class="spinner"></div>`;
-// intentionally wait 2000ms, for slower devices to ensure the stylesheet has loaded
-new Promise(resolve => setTimeout(resolve, 2000)).then(() => {
+
 fetch('https://japi.rest/discord/v1/application/757780137449226272')
     .then(res => {
         if (!res.ok) {
@@ -20,4 +19,10 @@ fetch('https://japi.rest/discord/v1/application/757780137449226272')
     .catch(err => {
         console.error("Currently in 40+ servers", err);
         servercount.innerHTML = "Currently in 40+ servers"; // whatever, i tried, this works
-    });})
+    });
+
+function aboutAv() {
+    msg(`<h2>What is ex3's avatar??</h2>ex3's avatar was created in 2023 by DALL-E 2. Before you go, "FUCKING AI SLOP!!!!", this image was created when AI generated media was pretty much
+        just shits and giggles; not polluting the internet. I have considered changing the avatar, but I am indefinitely keeping it to honor the days when AI generated photos were
+        unpredictable, funny, and basically, harmless.<br><img src="https://cdn.exerinity.com/images/ex3/home.png" style="width: 100px; height: 100px; margin-top: 10px;"><br><br>For complaints, click here: <a href="https://syndication.exerinity.com">https://syndication.exerinity.com</a>`);
+}
